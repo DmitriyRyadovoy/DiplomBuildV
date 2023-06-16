890d9a9458f92e3e9ae2e3e831da6b46e3dfa4ae
DmitriyRyadovoy <119165684+DmitriyRyadovoy@users.noreply.github.com>
2023-06-16 10:40:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, "./Client/dist")))
 
 //routes
 app.use("/api", router);
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 */
 
 app.use('*',function (req,res){
-  res.sendFile(path.join(__dirname, './client/dist/index.html'));
+  res.sendFile(path.join(__dirname, "./Client/dist/", "index.html"));
 })
 
 //PORT
